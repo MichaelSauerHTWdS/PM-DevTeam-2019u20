6fbd2ac3f7588e2d75210737825335b0324d2792
Lukas Roth <>
2020-01-23 14:05:16 +0100

    added Lukas Roth to file

--- menue.java
+++ menue.java
@@ -8,39 +8,44 @@
  * Compile:   javac menue.java
  * Execute:   java menu
  *
- * Testzeile fuer push aus KI-Labor cmd-Client 
+ * Testzeile fuer push aus KI-Labor cmd-Client
  */
 
 import java.util.Scanner;
 
 public class menue{
-        public static void main(String[] args) {
-        	final int PROGRAMMENDE=0;
-        	final int MICHAEL_SAUER=1;
-			final int MIKE_SOUR=2;
-        	boolean isEnde=false;
+	public static void main(String[] args) {
+		final int PROGRAMMENDE=0;
+		final int MICHAEL_SAUER=1;
+		final int MIKE_SOUR=2;
+		final int LUKAS_ROTH=3;
+		boolean isEnde=false;
 
-        	String[] textfeld={"Ciao",
-                                "Michael Sauer",
-                                "Mike Sour"};
+		String[] textfeld={"Ciao",
+				"Michael Sauer",
+				"Mike Sour",
+				"Lukas Roth"};
 
-        	while(!isEnde){
-                	Scanner scanner = new Scanner(System.in);
-                	int wahl = scanner.nextInt();
-                	switch (wahl) {
-                        	case PROGRAMMENDE:
-                                	System.out.println(textfeld[0]);
-                                	isEnde=true;
-                        	break;
-                        	case MICHAEL_SAUER:
-                                	System.out.println("Michael Sauer");
-                        	break;
-                        	case MIKE_SOUR:
-                                	System.out.println("Mike Sour");
-                        	break;
-                        	default:
-                                	System.out.println("Fehler: Kenne ich nicht!");
-                        	}
-                	}
-        	}
+		while(!isEnde){
+			Scanner scanner = new Scanner(System.in);
+			int wahl = scanner.nextInt();
+			switch (wahl) {
+				case PROGRAMMENDE:
+					System.out.println(textfeld[0]);
+					isEnde=true;
+					break;
+				case MICHAEL_SAUER:
+					System.out.println("Michael Sauer");
+					break;
+				case MIKE_SOUR:
+					System.out.println("Mike Sour");
+					break;
+				case LUKAS_ROTH:
+					System.out.println("Lukas Roth");
+					break;
+				default:
+					System.out.println("Fehler: Kenne ich nicht!");
+			}
+		}
 	}
+}
