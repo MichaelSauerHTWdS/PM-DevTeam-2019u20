f1a18b33410df0b164126c44589fb1c4fdf2bb52
PinknesEinhorn <pinkneseinhorn@gmail.com>
2020-01-23 14:23:13 +0100

    added case JANA_KOCH

--- menue.java
+++ menue.java
@@ -17,11 +17,14 @@ public class menue{
         public static void main(String[] args) {
         	final int PROGRAMMENDE=0;
         	final int MICHAEL_SAUER=1;
-        	boolean isEnde=false;
+        	final int MIKE_SOUR=2;
+		final int JANA_KOCH=3;
+		boolean isEnde=false;
 
         	String[] textfeld={"Ciao",
                                 "Michael Sauer",
-                                "Mike Sour"};
+                                "Mike Sour",
+				"Jana Koch"};
 
         	while(!isEnde){
                 	Scanner scanner = new Scanner(System.in);
@@ -30,13 +33,16 @@ public class menue{
                         	case PROGRAMMENDE:
                                 	System.out.println(textfeld[0]);
                                 	isEnde=true;
-                        	break;
+                        		break;
                         	case MICHAEL_SAUER:
                                 	System.out.println("Michael Sauer");
-                        	break;
+                        		break;
                         	case MIKE_SOUR:
                                 	System.out.println("Mike Sour");
-                        	break;
+                        		break;
+				case JANA_KOCH:
+					System.out.println("Jana Koch");
+					break;
                         	default:
                                 	System.out.println("Fehler: Kenne ich nicht!");
                         	}
